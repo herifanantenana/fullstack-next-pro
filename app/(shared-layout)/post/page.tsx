55c50faa3d88f3153a2c211fc5c9a67eddaae94d
herifanantenana <herifanantenana17>
2026-05-04 01:06:07 +0300

    feat: refactor PostPage to use Suspense for loading posts and add Skeleton component for loading state

--- app/(shared-layout)/post/page.tsx
+++ app/(shared-layout)/post/page.tsx
@@ -5,14 +5,14 @@ import {
 	CardFooter,
 	CardHeader,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default async function PostPage() {
-	const post = await fetchQuery(api.posts.getPosts);
-
+export default function PostPage() {
 	return (
 		<div className="space-y-12 py-12">
 			<div className="text-cen<<ter">
@@ -24,35 +24,64 @@ export default async function PostPage() {
 				</p>
 			</div>
 
-			<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-				{post?.map((post) => (
-					<Card key={post._id} className="pt-0">
-						<CardHeader className="relative h-48 overflow-hidden rounded-none">
-							<Image
-								src="https://images.unsplash.com/photo-1768839726129-8dcb29a4e7b8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-								alt="Nextjs 16"
-								fill
-								className="rounded-lg object-cover"
-							/>
-						</CardHeader>
+			<Suspense fallback={<SkeletonPostUI />}>
+				<PostList />
+			</Suspense>
+		</div>
+	);
+}
 
-						<CardContent>
-							<Link href={`/post/${post._id}`}>
-								<h2 className="hover:text-primary text-2xl font-semibold transition-colors duration-200">
-									{post.title}
-								</h2>
-							</Link>
-							<p className="text-muted-foreground line-clamp-3">{post.body}</p>
-						</CardContent>
+async function PostList() {
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	const post = await fetchQuery(api.posts.getPosts);
 
-						<CardFooter>
-							<Button asChild className="w-full">
-								<Link href={`/post/${post._id}`}>Read More</Link>
-							</Button>
-						</CardFooter>
-					</Card>
-				))}
-			</div>
+	return (
+		<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+			{post?.map((post) => (
+				<Card key={post._id} className="pt-0">
+					<CardHeader className="relative h-48 overflow-hidden rounded-none">
+						<Image
+							src="https://images.unsplash.com/photo-1768839726129-8dcb29a4e7b8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+							alt="Nextjs 16"
+							fill
+							className="rounded-lg object-cover"
+						/>
+					</CardHeader>
+
+					<CardContent>
+						<Link href={`/post/${post._id}`}>
+							<h2 className="hover:text-primary text-2xl font-semibold transition-colors duration-200">
+								{post.title}
+							</h2>
+						</Link>
+						<p className="text-muted-foreground line-clamp-3">{post.body}</p>
+					</CardContent>
+
+					<CardFooter>
+						<Button asChild className="w-full">
+							<Link href={`/post/${post._id}`}>Read More</Link>
+						</Button>
+					</CardFooter>
+				</Card>
+			))}
+		</div>
+	);
+}
+
+function SkeletonPostUI() {
+	return (
+		<div className="grid gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+			{Array.from({ length: 6 }).map((_, index) => (
+				<div key={index} className="space-y-4">
+					<Skeleton className="h-48 rounded-xl" />
+					<div className="space-y-2">
+						<Skeleton className="h-4 w-2/5" />
+						<Skeleton className="h-3 w-full" />
+						<Skeleton className="h-3 w-3/4" />
+					</div>
+					<Skeleton className="h-10 w-full" />
+				</div>
+			))}
 		</div>
 	);
 }
